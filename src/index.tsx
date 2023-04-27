7aa6289a08c00db32f312542e7fc0c062532d968
Henry Burgess <henryjburg@gmail.com>
2023-04-27 12:14:53 -0500

    Add stimuli

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,39 @@ import { Experiment } from "neurocog";
 
 // Import jsPsych plugins
 import "jspsych/plugins/jspsych-instructions";
+import "jspsych/plugins/jspsych-html-keyboard-response";
 
-// Import the custom plugin before adding it to the timeline
-import "src/plugin";
+// Import custom styling
+import "./css/styles.css";
 
 // Create a new Experiment instance
-const experiment = new Experiment(Configuration);
+export const experiment = new Experiment(Configuration);
 
 // Timeline setup
 const timeline = [];
 
-timeline.push({
-  type: Configuration.studyName,
-});
+const generateTrial = (): any[] => {
+  const stimuli = [];
+
+  // Generate random duration for fixation cross
+  const duration = Math.round(1500 + experiment.random() * 1000);
+
+  // Fixation cross
+  stimuli.push({
+    type: "html-keyboard-response",
+    stimulus: '<div style="font-size:60px;">+</div>',
+    choices: "NO_KEYS",
+    trial_duration: duration,
+  });
+
+  // Centrally presented stimulus
+
+  // LVF or RVF stimulus
+
+  return stimuli;
+};
+
+timeline.push(...generateTrial());
 
 // Configure and start the experiment
 experiment.start({
